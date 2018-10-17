2475293ac3aa34ee4e4fb800052f08a1cc069140
Yoyok Tri Wicaksono <yoyok.thewicaksono@gmail.com>
2018-10-17 17:01:03 +0700

    update swagger

--- src/main/java/org/scm/ojt/rest/module/ApiModule.java
+++ src/main/java/org/scm/ojt/rest/module/ApiModule.java
@@ -1,6 +1,7 @@
 package org.scm.ojt.rest.module;
 
 import org.scm.ojt.rest.config.ConfigurationManager;
+import org.scm.ojt.rest.config.SwaggerConfigData;
 import org.scm.ojt.rest.dao.ConnectionManager;
 import org.scm.ojt.rest.filter.CrossOriginResourceSharingFilter;
 import org.scm.ojt.rest.servlet.ApiApplication;
@@ -9,6 +10,8 @@ import com.google.inject.servlet.ServletModule;
 import io.swagger.jaxrs.config.BeanConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.scm.ojt.rest.utils.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,16 +20,17 @@ import java.util.Map;
  * @author cluttered.code@gmail.com
  */
 public class ApiModule extends ServletModule {
-
+    private static final Logger LOG = LoggerFactory.getLogger(ApiModule.class);
 
     @Override
     protected void configureServlets() {
+        LOG.info("Configuring Servlets...");
         bind(ConnectionManager.class).asEagerSingleton();
         bind(ServletContainer.class).asEagerSingleton();
         bind(CrossOriginResourceSharingFilter.class).asEagerSingleton();
 
         initializeApplicationServlet();
-        initializeSwaggerBootstrap();
+        initializeSwaggerBootstrap(ConfigurationManager.getInstance().getSwaggerConfigData());
 
         // CORS Filter
         filter("/*").through(CrossOriginResourceSharingFilter.class);
@@ -39,13 +43,14 @@ public class ApiModule extends ServletModule {
         serve("/v1/*").with(ServletContainer.class, props);
     }
 
-    private void initializeSwaggerBootstrap() {
+    private void initializeSwaggerBootstrap(SwaggerConfigData swaggerConfigData) {
+        LOG.info("Configuring Swagger...");
         serve("").with(SwaggerBootstrap.class);
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0-SNAPSHOT");
+        beanConfig.setVersion(swaggerConfigData.version());
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setTitle("Todo API");
-        beanConfig.setDescription("Description of Todo API");
+        beanConfig.setTitle(swaggerConfigData.title());
+        beanConfig.setDescription(swaggerConfigData.description());
         beanConfig.setHost(ConfigurationManager.getInstance().getAppConfigData().host() + ":" + ConfigurationManager.getInstance().getAppConfigData().port().toString());
         beanConfig.setBasePath("/api/v1");
         beanConfig.setResourcePackage(AppConstants.SERVICEPACKAGE);
