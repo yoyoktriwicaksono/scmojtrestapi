f79f67bc75663334f1126f9b42b610a46eaca321
Yoyok Tri Wicaksono <yoyok.thewicaksono@gmail.com>
2018-10-19 12:47:47 +0700

    add customerservicetest

---
+++ src/test/java/org/scm/ojt/rest/service/CustomerServiceTest.java
@@ -0,0 +1,24 @@
+package org.scm.ojt.rest.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Yoyok_T on 19/10/2018.
+ */
+public class CustomerServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testFirst(){
+        String test = "test";
+        assertNotNull(test);
+    }
+
+}
