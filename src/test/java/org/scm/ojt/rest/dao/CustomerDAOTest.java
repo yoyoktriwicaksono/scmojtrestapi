d7d63757bce049cfc075a8720c438163a86b6093
Yoyok Tri Wicaksono <yoyok.thewicaksono@gmail.com>
2018-10-19 14:40:43 +0700

    update test

--- src/test/java/org/scm/ojt/rest/dao/CustomerDAOTest.java
+++ src/test/java/org/scm/ojt/rest/dao/CustomerDAOTest.java
@@ -2,6 +2,9 @@ package org.scm.ojt.rest.dao;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mongodb.morphia.Datastore;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -9,8 +12,13 @@ import static org.junit.Assert.assertNotNull;
  * Created by Yoyok_T on 19/10/2018.
  */
 public class CustomerDAOTest {
+    private Datastore datastore;
+    private CustomerDAO customerDAO;
+
     @Before
     public void setUp() throws Exception {
+        datastore = Mockito.mock(Datastore.class);
+        customerDAO = new CustomerDAO(datastore);
 
     }
 
